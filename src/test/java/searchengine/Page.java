f7fafba2ca7a184e1c9040e65e1dccaee2e2f895
Ewa Habierska <ewahabierska@MacBook-Air-Ewa-3.local>
2019-11-20 16:51:05 +0100

    test

--- src/test/java/searchengine/Page.java
+++ src/test/java/searchengine/Page.java
@@ -2,9 +2,9 @@
 // second comment
 //third comment
 //Main comment
-public class Page
-{
-    private int count;
-    private String title;
-    private Array
-}
+// public class Page
+// {
+//     private int count;
+//     private String title;
+//     private Array
+// }
