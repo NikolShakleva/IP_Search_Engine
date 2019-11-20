34489f4b972065f6fac8bc3370fc03be73b782e8
unknown <nikv@itu.dk>
2019-11-20 17:06:21 +0100

    class Responder is created

---
+++ Page.java
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+=======
+package searchengine;
+
+import java.util.ArrayList;
+
+>>>>>>> Emelie
+// my comment
+// second comment
+//third comment
+//Main comment
+public class Page
+{
+    private int count;
+    private String title;
+<<<<<<< HEAD
+    private Array
+}
+=======
+    private ArrayList<String> list;
+}
+>>>>>>> Emelie
