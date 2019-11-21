0f3478ae84cc0688be80ebd14e9e3fef7b10bc73
Ewa Habierska <ewahabierska@MacBook-Air-Ewa-3.local>
2019-11-21 18:45:02 +0100

    Responder class in the Main branch updated

--- src/main/java/searchengine/Responder.java
+++ src/main/java/searchengine/Responder.java
@@ -1,5 +1,21 @@
 package searchengine;
 
-class Responder {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Responder
+ */
+public class Responder {
+
+
+    public Responder(List list)   {
+
+    }
+
+    List<String> getPageNames(){
+      List<String> pageNames = new ArrayList<>();
+
+      return pageNames;
+    }
 }
