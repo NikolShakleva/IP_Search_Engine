5e2f6e15482e0e1e8414abf41a0f281d46256d53
Ewa Habierska <ewahabierska@MacBook-Air-Ewa-3.local>
2019-11-21 18:42:52 +0100

    Library class in the Main class updated

--- src/main/java/searchengine/Library.java
+++ src/main/java/searchengine/Library.java
@@ -1,9 +1,31 @@
 package searchengine;
 
 /**
+package searchengine;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
  * Library
  */
 public class Library {
+    private ArrayList<Page> library;
 
+    public Library()
+    {
+        library = new ArrayList<Page>();
+        Collections.reverse(library);
+    }
+
+    public void add(Page page)
+    {
+        library.add(page);
+    }
     
 }
