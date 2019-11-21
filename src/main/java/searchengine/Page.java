4f99cb7205b33f73c8fa732b9b9d332638497ff3
Ewa Habierska <ewahabierska@MacBook-Air-Ewa-3.local>
2019-11-21 18:41:34 +0100

    Page class in the Main branch updated

---
+++ src/main/java/searchengine/Page.java
@@ -0,0 +1,35 @@
+package searchengine;
+import java.util.*;
+
+public class Page{
+    private int relevance;
+    private ArrayList<String> words;
+    private String name;
+    private String title;
+
+    public Page(String pageName, String pageTitle, ArrayList<String> pageWords){
+        this.words = pageWords;
+        this.title = pageTitle;
+        this.name = pageName;
+        relevance = 0;
+
+
+    }
+
+    public boolean check(String input){
+        if (words.contains(input)){
+            this.relevance++;
+            return true;   
+        }else return false;
+       }
+    public String getName(){
+        return this.name;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public int getRelevance(){
+    return this.relevance;
+    }
+}
+        
