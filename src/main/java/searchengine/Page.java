0e632a6ae9da66f2dc951077c0d94e4d9700a7bd
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2019-11-24 17:06:57 +0100

    pages and query

--- src/main/java/searchengine/Page.java
+++ src/main/java/searchengine/Page.java
@@ -1,3 +1,38 @@
 package searchengine;
 import java.util.*;
 
+public class Page{
+    private int relevance;
+    private ArrayList<String> words;
+    private String url;
+    private String title;
+
+    public Page(String pageUrl, String pageTitle, ArrayList<String> pageWords){
+        this.words = pageWords;
+        this.title = pageTitle;
+        this.url = pageUrl;
+        relevance = 0;
+       // System.out.println(words);
+
+
+    }
+
+    public boolean check(String input){
+        if (words.contains(input)){
+            this.relevance++;
+            return true;   
+        }else return false;
+       }
+       public ArrayList<String> getWords()  {
+           return words;
+       }
+    public String getUrl(){
+        return this.url;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public int getRelevance(){
+    return this.relevance;
+    }
+}
--- src/main/java/searchengine/Query.java
+++ src/main/java/searchengine/Query.java
@@ -1,37 +1,23 @@
 package searchengine;
 import java.util.*;
 
-import java.util.List;
-
 /**
  * Query
- * 
- * @author Ewa, Emelie, Nikol, Philip
- * @version 2019.11.24
  */
- class Query {
-     String searchTerm;
-     List<Page> matchingList;
-
-     Query(String searchTerm)  {
-       this.searchTerm = searchTerm;
-
-
+public class Query {
+   // private ArrayList<String> wrongWords;
     private ArrayList<Page> relevantPages;
     private Library library;
 
     public Query(String input, Library library){
         this.library = library;
         relevantPages = library.matchingPages(input);
+       
     }
 
-    /**
-     * 
-     * @return, ArrayList of pages matching the search Term
-     */
-
-    public ArrayList<Page> getCorrectPages()    {
+    public ArrayList<Page> getCorrectPages(){
         return this.relevantPages;
+
     }
 
 
