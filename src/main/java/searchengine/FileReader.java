f435b6b4fbc0158e16b80f79f8224b5aadf6890c
unknown <nikv@itu.dk>
2019-12-10 18:11:00 +0100

    notations of the Page and FileReader class

--- src/main/java/searchengine/FileReader.java
+++ src/main/java/searchengine/FileReader.java
@@ -6,55 +6,65 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * FileReader Class
+ * Class FileReader - read a  text file. While reading the file, initatites page objects.
+ *
+ * This class is part of the "WebServer" application.  
  * 
- * @author Emelie, Ewa, Nikol, Philip
- * @version 2019.11.24
- * 
+ * @author  Emelie, Philip, Ewa, Nikol
+ * @version 2019.11.20
  */
 public class FileReader {
 
     private ArrayList<Page> allPages;
 
+    /**
+     * Initaiates arrayList of pages
+     * Read the text file
+     * It creates page objects while reading the file.
+     * Stores the page objects into the arrayList of pages
+     * @param filename The name of the text file, coming from the WebServer class.
+     */
     public FileReader(String filename)  {
         allPages = new ArrayList<>();
         readFile(filename);
     }
     /**
-     * Reading the file given from the webserver and creates pages that are added to an arraylist
+     * Reading the file given from the webserver and creates pages that are added to an arrayList of pages
      * 
      * @param filename, filename of text file with websites containing url, title and words
      */
     public void readFile(String filename)   {     
         try(Scanner sc = new Scanner(new File(filename));) {
             String line = sc.nextLine();
-            while (sc.hasNextLine())
+            while (sc.hasNextLine()) // check if the file contains more words
             {
-                ArrayList<String> words = new ArrayList<>();
-                    if(line.startsWith("*PAGE")) 
+                ArrayList<String> words = new ArrayList<>(); // initialises empthy arrayList of words for each page
+                    if(line.startsWith("*PAGE"))            // checks if the line contains URL
                     {
-                        String pageUrl = line.replace("*PAGE:", "");
-                        String title = sc.nextLine();
-                            while (sc.hasNextLine())
+                        String pageUrl = line.replace("*PAGE:", ""); //assignes a URL to the page object
+                        String title = sc.nextLine();      // assigns a title to the page object 
+                            while (sc.hasNextLine())      // checks if the page contains any words
                             {
-                             String word = sc.nextLine();
+                             String word = sc.nextLine();  
                              if(!word.startsWith("*PAGE"))
                                 {
-                                    words.add(word);
+                                    words.add(word);    // fills the arrayList of words for each page object
                                 } 
-                                else 
+                                else                    // when a page contains no more words, stop adding words to the arraylist of words
                                 {
                                     line = word;
                                     break;
                                 }
                             }
-                            if (!words.isEmpty())
+                            if (!words.isEmpty())      // if the arrayList of words contains words a page object is created
                             {
                                 Page pt = new Page(pageUrl,title, words);
                                 allPages.add(pt); 
                             }
                     } 
-                    else 
+                    else        // if the line doesn't start with a *PAGE
+                               // the reading of the file is terminated
+
                     {
                         line = "Error";
                     }
@@ -63,9 +73,8 @@ public class FileReader {
             e.printStackTrace();   
     }  }
         /**
-         * 
-         * 
-         * @return, ArrayList with page objects
+         * Return the arraylist of pages - allPages, after the file has been read
+         * @return, the full ArrayList of page objects
          */
         public ArrayList<Page> getAllPages() {
             return allPages;
--- src/main/java/searchengine/Page.java
+++ src/main/java/searchengine/Page.java
@@ -2,7 +2,11 @@ package searchengine;
 import java.util.*;
 
 /**
- * Page
+ * Class Page - is the blueprint of a page object.
+ * It describes all companents f a page object
+ * It calculates the Simple count score relevance of a word in a page
+ * It calculates the TF (term frequency) relevance of a word in a page
+ * It calculates the TFIDF (term frequency - inverted document frequency) relevance of a word in a page
  * 
  * @author Emelie, Ewa, Nikol, Philip
  * @version 2019.11.24
@@ -16,7 +20,13 @@ public class Page{
     private HashMap<String, Double> tfIdf;
 
 
-
+/**
+ * Create a page object based on the paramerts passed to the constructor
+ * It creates a map calculating the term-frequency relevance of each word in the page objects
+ * @param pageUrl Creates the page URL
+ * @param pageTitle Creates the page title
+ * @param words Creates a list of words contained in a page
+ */
     public Page(String pageUrl, String pageTitle, ArrayList<String> words){
         this.title = pageTitle;
         this.url = pageUrl;
@@ -25,66 +35,96 @@ public class Page{
 
     }
        /**
-        * 
-        * @return, the pages' URL as a String
+        * @return The pages' URL as a String
         */
         public String getUrl()  {
             return this.url;
         }
+
         /**
-         * 
-         * @return, the page Title as a String
+         * @return The page Title as a String
          */
         public String getTitle()    {
             return this.title;
         }
 
+        /**
+         * @return The ArayList of words contained in a single page
+         */
         public ArrayList<String> getWords() {
             return words;
         }
 
+        /**
+         * Create a map which calculates the term-frequenct ratio (TFR) of each words in the page object
+         * The word in each page is the key of the tf map. 
+         * The term-frequenct ratio (TFR) is the value of the tf map
+         * @return tf as a HashMap of String and Double
+         */
         public HashMap<String, Double> makeTF()
         {
             tf = new HashMap<>();
-            for(String singleWord : words)
+            for(String singleWord : words) // iterates over each word into the list of words in every page object
             {
-            double occurences = Collections.frequency(words, singleWord);
+            double occurences = Collections.frequency(words, singleWord); // calculates the number of times a words exist in a page object
             double allWords = words.size();
-            double tfRatio = occurences / allWords;
+            double tfRatio = occurences / allWords; // calculate the term frequency ratio (TFR) of each word in a page object
             tf.put(singleWord, tfRatio);
             }
 
             return tf;
         }
 
+        /**
+         * Calculates the tfidf relevance of each word in a page object and stores the result in a TFIDF HashMap <String, Double>
+         * wordKey is the key
+         * current relevance is the value
+         * @param idfFromIndex This is a HashMap<String, Double> which holds the inverse document frequency (IDF) of each word in all page objects 
+         */
         public void TFIDFCalculation(HashMap<String , Double> idfFromIndex)    {
-            tfIdf = new HashMap<>();
+            tfIdf = new HashMap<>(); // map which stores the term frequency-inerted document frequency (TFIDF) for each word in a page object is initaiated
             for(String wordKey : tf.keySet())
             {
-                double currentWordRelevance = tf.get(wordKey) * idfFromIndex.get(wordKey);
+                double currentWordRelevance = tf.get(wordKey) * idfFromIndex.get(wordKey); //calculate the currentRelevance be the formula: TF*IDF
                 tfIdf.put(wordKey, currentWordRelevance);
             }
         }
 
+        /** The method returns the TFIDF relevance score of a given word from the Query class
+        * @param word The words which relevance score is obtained from the TFIDF map
+        * @return The TFIDF score relevance of of a given word in double
+        */
         public double getRelevanceTFIDF(String word) {
             double wordRelevence = tfIdf.get(word);
-            if(title.contains(word))  {
+            if(title.contains(word))  {  // if the words is a title of the page, the relevance score is doubled
                 wordRelevence += tfIdf.get(word);
             }
             return wordRelevence;
         }
+
+        /** The method returns the simple count relevance score of a given word from the Query class
+        * @param word The words which relevance score is obtained based on count frequency 
+        * @return The simple score relevance of of a given word in double
+        */
+        
         public double getRelevanceSimple(String word) {
             double wordRelevence = Collections.frequency(words, word);
-            if(title.contains(word))  {
+            if(title.contains(word))  {  // if the words is a title of the page, the relevance score is doubled
                 wordRelevence += tfIdf.get(word);
             }
             return wordRelevence;
         }
+
+         /** The method returns the TF relevance score of a given word from the Query class
+        * @param word The words which relevance score is obtained from the TF map
+        * @return The TF score relevance of of a given word in double
+        */
         public double getRelevanceTF(String word) {
             double wordRelevence = tf.get(word);
-            if(title.contains(word))  {
+            if(title.contains(word))  {   // if the words is a title of the page, the relevance score is doubled
                 wordRelevence += tfIdf.get(word);
-            }return wordRelevence;
+            }
+            return wordRelevence;
             
         }
 }
