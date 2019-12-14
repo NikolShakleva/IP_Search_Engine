a589e818b6a14198a408f7f45fdc37b5bfb3711e
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2019-12-15 00:51:15 +0100

    webst

--- src/test/java/searchengine/WebServerTest.java
+++ src/test/java/searchengine/WebServerTest.java
@@ -16,13 +16,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 
-@TestInstance(Lifecycle.PER_CLASS)
 class WebServerTest {
+    int PORT = 8080;
 
     @Test
     void lookupWebServerWithSimple() {
 
-          var  server = new WebServer(8080, "test-file1.txt","hash", "simple");
+          var  server = new WebServer(PORT, "test-file1.txt","hash", "simple");
     
         String baseURL = String.format("http://localhost:%d/search?q=", server.server.getAddress().getPort());
         assertEquals("[{\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"2.0\", \"totalWords\": \"3\", \"words\": \"[word1, word2, word1]\"}, {\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"1.0\", \"totalWords\": \"3\", \"words\": \"[word1, word3, word3]\"}]", 
@@ -33,12 +33,13 @@ class WebServerTest {
             httpGet(baseURL + "word3"));
         assertEquals("[]", 
             httpGet(baseURL + "word4"));
+            server.server.stop(0);
     }
 
     @Test
     void lookupWebServerWithTf() {
    
-                var server = new WebServer(8080, "test-file1.txt","hash", "tf");
+                var server = new WebServer(PORT, "test-file1.txt","hash", "tf");
     
         String baseURL = String.format("http://localhost:%d/search?q=", server.server.getAddress().getPort());
         assertEquals("[{\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"0.6666666666666666\", \"totalWords\": \"3\", \"words\": \"[word1, word2, word1]\"}, {\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"0.3333333333333333\", \"totalWords\": \"3\", \"words\": \"[word1, word3, word3]\"}]", 
@@ -49,21 +50,23 @@ class WebServerTest {
             httpGet(baseURL + "word3"));
         assertEquals("[]", 
             httpGet(baseURL + "word4"));
+            server.server.stop(0);
     }
 
     @Test
     void lookupWebServerWithTFIDF() {
-      var server = new WebServer(8080, "test-file1.txt","hash", "tfidf");
+      var server = new WebServer(PORT, "test-file1.txt","hash", "tfidf");
     
         String baseURL = String.format("http://localhost:%d/search?q=", server.server.getAddress().getPort());
-        assertEquals("[{\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"0.0\", \"totalWords\": \"2\", \"words\": \"[word1, word2]\"}, {\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"0.0\", \"totalWords\": \"2\", \"words\": \"[word1, word3]\"}]", 
+        assertEquals("[{\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"0.0\", \"totalWords\": \"3\", \"words\": \"[word1, word3, word3]\"}, {\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"0.0\", \"totalWords\": \"3\", \"words\": \"[word1, word2, word1]\"}]", 
             httpGet(baseURL + "word1"));
-        assertEquals("[{\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"0.34657359027997264\", \"totalWords\": \"2\", \"words\": \"[word1, word2]\"}]",
+        assertEquals("[{\"url\": \"http://page1.com\", \"title\": \"title1\", \"relevance\": \"0.23104906018664842\", \"totalWords\": \"3\", \"words\": \"[word1, word2, word1]\"}]",
             httpGet(baseURL + "word2"));
-        assertEquals("[{\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"0.34657359027997264\", \"totalWords\": \"2\", \"words\": \"[word1, word3]\"}]", 
+        assertEquals("[{\"url\": \"http://page2.com\", \"title\": \"title2\", \"relevance\": \"0.46209812037329684\", \"totalWords\": \"3\", \"words\": \"[word1, word3, word3]\"}]", 
             httpGet(baseURL + "word3"));
         assertEquals("[]", 
             httpGet(baseURL + "word4"));
+            server.server.stop(0);
     }
 
     private String httpGet(String url) {
