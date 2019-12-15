0114aa72a398a385c72a1562562b6f3855742b3c
Ewa Habierska <ewahabierska@MacBook-Pro.local>
2019-12-15 15:27:19 +0100

    hashmap test

--- src/test/java/searchengine/IndexHashTest.java
+++ src/test/java/searchengine/IndexHashTest.java
@@ -22,7 +22,7 @@ public class IndexHashTest {
      */
     @BeforeEach
     public void setUp()   {
-   
+
        for(int i = 0 ; i < 10 ; i++)  {
             String title = "title" + i;
             String pageUrl = "*PAGE https://www." + title + ".com";
@@ -35,41 +35,35 @@ public class IndexHashTest {
             var page = new Page(pageUrl, title, words);
             allPages.add(page);
         } 
+
         var words = new ArrayList<String>();
         words.add("word1");
+        words.add("word1");
+        words.add("word2");
         String pageUrl = "*PAGE https://www.title.com";
         var page = new Page(pageUrl, "title", words);
         allPages.add(page);
-
     }
 
     @Test
-    public void makeEmptyHashMap()  {
-        ArrayList<Page> emptyHashMap = new ArrayList<>();
-        Index ind = new IndexHash(emptyHashMap);
-        int wordsTP = ind.getwordsToPages().keySet().size();
-        assertEquals(wordsTP, 0);
-    }
-
-    @Test
-    public void makeHashM()  {
+    public void checksIfTheHashMapIsCreatedAndReturnedProperly()  {
         Index ind = new IndexHash(allPages);
-        int wordsTP = ind.getwordsToPages().keySet().size();
-        assertEquals(wordsTP, 10);
+        int numberOfKeyWords = ind.getwordsToPages().keySet().size();
+        assertEquals(numberOfKeyWords, 10);
     }
 
     @Test
-    public void getAllPagesTest()   {
+    public void checksIfReturnesAllPagesFromTheFile()   {
         Index ind = new IndexHash(allPages);
         int allPagesNB = ind.getAllPages().size();
         assertEquals(allPagesNB, 11);
     }
 
     @Test
-    public void matchingPagesTest() {
+    public void checksIfReturnesRightNumberOfPagesThatContainAKeyWord() {
         Index ind = new IndexHash(allPages);
         int matchingPage = ind.matchingPages("word1").size();
         assertEquals(matchingPage, 2);
-
-    }    
+    }  
+    
 }
